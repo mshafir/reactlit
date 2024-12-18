d26deca27679f5eea0ad7ea1548c615af97142a9
Andy <volleyvolley321@hotmail.com>
2024-12-18 21:48:35 +0100

    fix ssr issue

--- apps/reactlit-examples/src/components/theme-toggle.tsx
+++ apps/reactlit-examples/src/components/theme-toggle.tsx
@@ -1,10 +1,21 @@
 'use client';
+import { useEffect, useState } from 'react';
 import { Button, Tooltip } from '@radix-ui/themes';
 import { MoonIcon, SunIcon } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 export function ThemeToggle() {
   const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <Tooltip content="Toggle theme">
       <Button
