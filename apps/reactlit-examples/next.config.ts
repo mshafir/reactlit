ac4dcb3e86c39183f27e4f9d78407ce4068c4fa5
Michael Shafir <michael.shafir@gmail.com>
2025-04-25 10:44:39 -0400

    update pnpm

--- apps/reactlit-examples/next.config.ts
+++ apps/reactlit-examples/next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
 	output: "export",
 	transpilePackages: ["@reactlit/core", "@reactlit/vanilla"],
 	reactStrictMode: true,
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
 };
 
 export default nextConfig;
